0e061961d7001a284c7a5ee6242a4e4e814c4602
Ben Cordero <bencord0@condi.me>
2016-11-07 23:41:03 +0000

    Add GPG encrypted secrets

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 set -ex -o pipefail
 
+_SECRETS="
+  vars/secrets.yml
+"
+
+decrypt_secrets() {
+  for sec in $_SECRETS; do
+    gpg -d "${sec}".gpg > "${sec}"
+  done
+}
+
+destroy_secrets() {
+  for sec in $_SECRETS; do
+    rm -fv "${sec}"
+  done
+}
+
+trap destroy_secrets EXIT
+decrypt_secrets
+
 ansible-playbook -i inventory site.yml
